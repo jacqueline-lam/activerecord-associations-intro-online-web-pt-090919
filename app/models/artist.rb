0305e68b9ab232c921f4b881eb55e417e8e651b9
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-02 06:23:47 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
   # songs table has an artist_id column + Artist class uses has_many macro
-  # artist hasmany song
+  # artist has many song
   has_many :songs
   has_many :genres, through: :songs
 end
