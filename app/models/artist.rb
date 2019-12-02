3ed9a3995fc65dc4d00781bfd661f38473aedaeb
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-02 06:48:56 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,6 @@
 class Artist < ActiveRecord::Base
+  # songs table has an artist_id column + Artist class uses has_many macro
+  # artist has many song
+  has_many :songs
+  has_many :genres, through: :songs
 end
--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
-end
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
+endset_fire_to_the_rain = Song.new(name: "Set Fire to the Rain")
